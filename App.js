e3f7f73ee0682b2ccda20774f0f735d379195906
Ray Louis <x1337c@gmail.com>
2021-12-12 23:14:58 -0800

    fuckin shit up

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as FileSystem from "expo-file-system";
+
 
 export default function App() {
+
+  const [huh, setHuh] = React.useState("this a hook.")
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!11</Text>
+      <Text>Open up App.js to start working on your app!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!11</Text>
       <StatusBar style="auto" />
+      <Text>{huh}</Text>
+      <Button title="click me lol" onPress={() => {
+        setHuh("bru")
+        readShit()
+      }} />
     </View>
   );
+}
+
+async function readShit() {
+
+  const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+
+  if (permissions.granted) {
+    // Gets SAF URI from response
+    const uri = permissions.directoryUri;
+
+    // Gets all files inside of selected directory
+    // const files = await StorageAccessFramework.readDirectoryAsync(uri);
+    // alert(`Files inside ${uri}:\n\n${JSON.stringify(files)}`);
+
+    const contentsOfDocumentDirectory = FileSystem.getInfoAsync(uri)
+    contentsOfDocumentDirectory.then((contents) => {
+      console.log(contents)
+      // setHuh(contents)
+    }
+    ).catch((err) => {
+      console.error(err)
+    })
+  }
 }
 
 const styles = StyleSheet.create({
