39e868dc61614819b70afa749f2aa808233aef50
Ray Louis <x1337c@gmail.com>
2021-12-13 18:38:45 -0800

    idk

--- App.js
+++ App.js
@@ -5,16 +5,18 @@ import * as FileSystem from "expo-file-system";
 
 
 export default function App() {
-
+  
   const [huh, setHuh] = React.useState(0)
   const [m3uFiles, setM3uFiles] = React.useState([])
 
+  
+  
   return (
     <View style={styles.container}>
       <Text>!!!!!!11</Text>
       <StatusBar style="auto" />
       <Text>{huh}</Text>
-      <Button title="click me" onPress={() => {
+      <Button title="click me to fetch playlists" onPress={() => {
         setHuh(oldValue => oldValue + 1)
         m3uFilesPromise = fetch_m3u()
         m3uFilesPromise.then((m3u) => {
@@ -24,12 +26,28 @@ export default function App() {
         })
       }} />
       <Text>{JSON.stringify(m3uFiles)}</Text>
+      <Button title="click me to upload playlists to google drive"></Button>
     </View>
   );
 }
 
-async function fetch_m3u() {
+/**
+ * for each m3u file found:
+ * obtain the raw text of the file 
+ * put that raw text into the body of an upload to google drive, get back the id
+ * use the id to name the file appropriately 
+ */
 
+async function postRequest(url, data) {
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: data
+  });
+  return response;
+}
+
+async function fetch_m3u() {
   const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
   if (permissions.granted) {
     // Gets SAF URI from response
